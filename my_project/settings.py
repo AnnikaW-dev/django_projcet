ed3f510a2d472a5f63cff8d730f22a9a8dfd3c67
AnnikaW-dev <xwtuwqcj@students.codeinstitute.net>
2025-04-15 12:56:42 +0200

    Update files and settings for deploy on Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%dij@0ub24pk#mw@n1ghf8v+oo237qap$9ksdlgy_(p+)i-9@q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost',]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'hello_world',
+    'about',
 ]
 
 MIDDLEWARE = [
